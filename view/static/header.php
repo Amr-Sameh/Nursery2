99e7bd965406c53a32c12538cde5c089ad9c7edb
Omer Mohamed Rashad <Omer Mohamed Rashad>
2017-04-25 18:15:54 +0200

    survey-design

--- view/static/header.php
+++ view/static/header.php
@@ -13,7 +13,8 @@ $id='s';
 <html>
 <head>
     <meta charset="utf-8">
-
+    <link rel="stylesheet" type="text/css" href="css/uikit-rtl.min.css">
+    <link rel="stylesheet" type="text/css" href="css/uikit.min.css">
     <link rel="stylesheet" href="css/bootstrap.css" >
     <link rel="stylesheet" href="css/bootstrap.css" >
     <link rel="stylesheet" href="css/animate.css" >
