79aa7b2dfd154966d3b3a39f4b2540b0d4c8f67a
MichaelMounir12 <mikaelmounir@gmail.com>
2017-04-26 02:52:54 +0200

    omar test survey

---
+++ database/survey.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: MeGaCrazy
+ * Date: 4/24/2017
+ * Time: 11:48 AM
+ */
+class survey
+{
+    private $connect;
+
+    function __construct($var)
+    {
+
+    }
+}
