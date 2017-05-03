be06257c76b61d2bc04ffca9e6fc003760e56174
MichaelMounir12 <mikaelmounir@gmail.com>
2017-05-03 15:25:13 +0200

    unnessary edit

--- view/Add_tech.php
+++ view/Add_tech.php
@@ -7,27 +7,10 @@
  */
 include_once 'static/header.php';
 include_once 'navbar.php';
-include_once ''
 ?>
 <html>
 <body>
-<div class="container">
-    <form class="form-group" action="" method="post">
-        <label for="name">Name</label>
-        <input type="text" class="col-xs-12">
-        <label for="level" class="col-xs-3">level</label>
-        <label for="class" class="col-xs-3">class</label>
-        <label for="subjects"class="col-xs-3">Subjects</label>
-        <select>
-            <option value=""></option>
-            <option></option>
-            <option></option>
-        </select>
 
-
-    </form>
-
-</div>
 
 
 
