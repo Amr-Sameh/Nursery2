21092e36c30a1f56258cc9fa7b75a6116a610d9b
Amr-Sameh <amrsameh.develop@gmail.com>
2017-05-05 11:38:49 +0200

    fix 2

--- view/js/panel.js
+++ view/js/panel.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
 
 
     $(".getstudentfromlevel").click(function (e) {
-      var levelid=this.id.substring(2);
+        var levelid=this.id.substring(2);
         $(".getstudent").click(function (e) {
             var id = this.id.substring(1);
             $.ajax({
@@ -23,10 +23,10 @@ $(document).ready(function () {
 
 
     if($("#levelTable").length != 0) {
-       getlevels();
+        getlevels();
     }
     if($("#classTable").length != 0) {
-       getClasses();
+        getClasses();
     }
 
 
@@ -42,64 +42,39 @@ $(document).ready(function () {
         });
     });
 
-  /*
-  *  add stud
-  * */
-  var panel_add_stud= $('#panel_add_stud');
-   panel_add_stud.submit(function (event) {
-      event.preventDefault();
-       $.ajax({
-          url:"../classes/panel_action.php",
-          method:"POST",
-          data:panel_add_stud.serialize()+"&action=AddStud",
-           success:function (data) {
-               if(data=="Operation Success"){
-                   alert(data);
-               }
-           }
-
-       });
-
-
-    });
-
-
-
-
-
 
 
 
     $('#add_level').click(function () {
-            var level_name = $('#panel_add_level_input').val();
+        var level_name = $('#panel_add_level_input').val();
 
         $.ajax({
             url: "../classes/panel_action.php",
             method: "POST",
             data: {action: 'addLevel',name:level_name},
             success: function (data) {
-              if(data=='true'){
-              getlevels();
+                if(data=='true'){
+                    getlevels();
 
-              }
+                }
 
             }
         });
     });
-     $('#add_class').click(function () {
-            var class_name = $('#panel_add_level_input').val();
-            var max_number = $('#panel_add_max_number_input').val();
-            var level_id = $('#panal_add_level_number_input').val();
+    $('#add_class').click(function () {
+        var class_name = $('#panel_add_level_input').val();
+        var max_number = $('#panel_add_max_number_input').val();
+        var level_id = $('#panal_add_level_number_input').val();
 
         $.ajax({
             url: "../classes/panel_action.php",
             method: "POST",
             data: {action: 'addClass',class_name:class_name,max_number:max_number,level_id:level_id},
             success: function (data) {
-              if(data=='true'){
-              getClasses();
+                if(data=='true'){
+                    getClasses();
 
-              }
+                }
 
             }
         });
@@ -124,7 +99,7 @@ $(document).on('click','.panel_delete_level',function () {
         data: {action: 'deleteLevel',id:level_id},
         success: function (data) {
 
-getlevels();
+            getlevels();
 
 
         }
@@ -143,7 +118,7 @@ $(document).on('click','.panel_delete_class',function () {
         data: {action: 'deleteClass',id:class_id},
         success: function (data) {
 
-getClasses();
+            getClasses();
 
 
         }
@@ -166,13 +141,13 @@ $(document).on('click','#level_info_Table_close',function () {
 
 
 $(document).on('click','.panel_edit_level',function () {
-var level = this.getAttribute('name');
-$('#myModalLabel').html(level);
+    var level = this.getAttribute('name');
+    $('#myModalLabel').html(level);
     document.getElementById('level_new_name').disabled=true;
 
-document.getElementById('level_new_name').value=level;
+    document.getElementById('level_new_name').value=level;
 
-getallsubjects(level);
+    getallsubjects(level);
 
 
 });
@@ -183,7 +158,7 @@ $('#level_new_name_toogle').click(function () {
 });
 
 $('#edit_level_multilist').on(function () {
-   alert($('#multilist').val());
+    alert($('#multilist').val());
 });
 
 
@@ -196,10 +171,10 @@ $('#update_level').click(function () {
             $('#myModalLabel').html(document.getElementById('level_new_name').value);
             getallsubjects(document.getElementById('myModalLabel').innerHTML);
             getlevels();
-       if (data=='true')
-            alert('Level Updated');
-       else
-           alert('some Error happened');
+            if (data=='true')
+                alert('Level Updated');
+            else
+                alert('some Error happened');
 
         }
     });
@@ -217,22 +192,21 @@ function getlevels() {
         }
     });
 }
-    function getlevelinfo(id) {
+function getlevelinfo(id) {
 
-        $.ajax({
-            url: "../classes/panel_action.php",
-            method: "POST",
-            data: {action: 'getLevelInfo',id:id},
-            success: function (data) {
+    $.ajax({
+        url: "../classes/panel_action.php",
+        method: "POST",
+        data: {action: 'getLevelInfo',id:id},
+        success: function (data) {
 
-                $('#level_info').html(data);
+            $('#level_info').html(data);
 
-            }
-        });
+        }
+    });
 
-    }
+}
 
-<<<<<<< HEAD
 function getallsubjects(level) {
     $.ajax({
         url: "../classes/panel_action.php",
@@ -240,60 +214,58 @@ function getallsubjects(level) {
         data: {action: 'Leveledit_getAllSub',level:level},
         success: function (data) {
             $('#edit_level_multilist').html(data);
-||||||| merged common ancestors
-=======
-    $(document).on('click','.panel_delete_class',function () {
 
-    var class_id=this.id;
-    class_id=class_id.substr(class_id.lastIndexOf('c')+1);
+            $(document).on('click','.panel_delete_class',function () {
 
-
-    $.ajax({
-        url: "../classes/panel_action.php",
-        method: "POST",
-        data: {action: 'deleteClass',class_id:class_id},
-        success: function (data) {
-
-getClasses();
+                var class_id=this.id;
+                class_id=class_id.substr(class_id.lastIndexOf('c')+1);
 
 
-        }
-    });
+                $.ajax({
+                    url: "../classes/panel_action.php",
+                    method: "POST",
+                    data: {action: 'deleteClass',class_id:class_id},
+                    success: function (data) {
 
-});
+                        getClasses();
 
 
-   /* $('#add_class').click(function () {
-            var class_name = $('#panel_add_class_input').val();
-            var max_number = $('#panal_add_max_number_input').val();
-            var level_name = $('#panal_add_level_name_input').val();
+                    }
+                });
 
-        $.ajax({
-            url: "../classes/panel_action.php",
-            method: "POST",
-            data: {action: 'addClass',class_name:class_name, level_name:level_name, max_student_number:max_number},
-            success: function (data) {
-              if(data=='true'){
-                   getClasses();
+            });
 
-              }
 
+            /* $('#add_class').click(function () {
+             var class_name = $('#panel_add_class_input').val();
+             var max_number = $('#panal_add_max_number_input').val();
+             var level_name = $('#panal_add_level_name_input').val();
+
+             $.ajax({
+             url: "../classes/panel_action.php",
+             method: "POST",
+             data: {action: 'addClass',class_name:class_name, level_name:level_name, max_student_number:max_number},
+             success: function (data) {
+             if(data=='true'){
+             getClasses();
+
+             }
+
+             }
+             });
+             });
+             */
+            function getClasses(){
+                $.ajax({
+                    url:"../classes/panel_action.php",
+                    method:"POST",
+                    data: {action:'getClasses'},
+                    success: function(data){
+                        $('#classTable').html(data);
+                    }
+                })
             }
-        });
-    });
-*/
-    function getClasses(){
-        $.ajax({
-            url:"../classes/panel_action.php",
-            method:"POST",
-            data: {action:'getClasses'},
-            success: function(data){
-                $('#classTable').html(data);
-            }
-        })
-    }
 
->>>>>>> a7a098a0ef886e840ebf5bccca33f73e872d4e8f
 
         }
     });
