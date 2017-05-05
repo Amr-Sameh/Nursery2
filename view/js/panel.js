c25075f5576fd73e8e4ef0986b73fbc02de7490c
Amr-Sameh <amrsameh.develop@gmail.com>
2017-05-05 12:27:58 +0200

    fix-conflict

--- view/js/panel.js
+++ view/js/panel.js
@@ -106,25 +106,6 @@ $(document).on('click','.panel_delete_level',function () {
     });
 
 });
-$(document).on('click','.panel_delete_class',function () {
-
-    var class_id=this.class_id;
-    class_id=class_id.substr(class_id.lastIndexOf('s')+1);
-
-
-    $.ajax({
-        url: "../classes/panel_action.php",
-        method: "POST",
-        data: {action: 'deleteClass',id:class_id},
-        success: function (data) {
-
-            getClasses();
-
-
-        }
-    });
-
-});
 
 
 $(document).on('click','.panel_view_level',function () {
@@ -211,62 +192,42 @@ function getallsubjects(level) {
     $.ajax({
         url: "../classes/panel_action.php",
         method: "POST",
-        data: {action: 'Leveledit_getAllSub',level:level},
+        data: {action: 'Leveledit_getAllSub', level: level},
         success: function (data) {
             $('#edit_level_multilist').html(data);
+        }
+    });
+}
 
-            $(document).on('click','.panel_delete_class',function () {
+$(document).on('click','.panel_delete_class',function () {
 
-                var class_id=this.id;
-                class_id=class_id.substr(class_id.lastIndexOf('c')+1);
-
-
-                $.ajax({
-                    url: "../classes/panel_action.php",
-                    method: "POST",
-                    data: {action: 'deleteClass',class_id:class_id},
-                    success: function (data) {
-
-                        getClasses();
+    var class_id=this.class_id;
+    class_id=class_id.substr(class_id.lastIndexOf('s')+1);
 
 
-                    }
-                });
+    $.ajax({
+        url: "../classes/panel_action.php",
+        method: "POST",
+        data: {action: 'deleteClass',id:class_id},
+        success: function (data) {
 
-            });
-
-
-            /* $('#add_class').click(function () {
-             var class_name = $('#panel_add_class_input').val();
-             var max_number = $('#panal_add_max_number_input').val();
-             var level_name = $('#panal_add_level_name_input').val();
-
-             $.ajax({
-             url: "../classes/panel_action.php",
-             method: "POST",
-             data: {action: 'addClass',class_name:class_name, level_name:level_name, max_student_number:max_number},
-             success: function (data) {
-             if(data=='true'){
-             getClasses();
-
-             }
-
-             }
-             });
-             });
-             */
-            function getClasses(){
-                $.ajax({
-                    url:"../classes/panel_action.php",
-                    method:"POST",
-                    data: {action:'getClasses'},
-                    success: function(data){
-                        $('#classTable').html(data);
-                    }
-                })
-            }
+            getClasses();
 
 
         }
     });
+
+});
+
+            function getClasses() {
+                $.ajax({
+                    url: "../classes/panel_action.php",
+                    method: "POST",
+                    data: {action: 'getClasses'},
+                    success: function (data) {
+                        $('#classTable').html(data);
+                    }
+                });
+
+
 }
